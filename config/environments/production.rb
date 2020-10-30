f565e7f6b3a91006a153c0f7faabc1c6ce1dcf59
Yuki Uno <48462721+usagrammer@users.noreply.github.com>
2020-10-30 15:14:34 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://18.178.150.52/cable'
+  config.action_cable.allowed_request_origins = [ 'http://18.178.150.52' ]
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
